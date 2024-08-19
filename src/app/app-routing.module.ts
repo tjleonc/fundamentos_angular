9408936df9f05f8a5fd628ad0f51ba2c9c72d109
CETECOM <cetecom_conce@duoc.cl>
2024-08-19 15:55:23 -0400

    está todo comentado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { QuienesSomosComponent } from './pages/quienes-somos/quienes-somos.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+  path: 'home',
+  component:HomeComponent
+  },
+  {
+    path:'contacto',
+    component:ContactoComponent
+  },
+  {
+    path:'quienes_somos',
+    component:QuienesSomosComponent
+  },
+  {
+    path:'**', //acá ponemos ** para que envie cada vez que la pagina no se encuentre
+    component:NotfoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ContactoComponent } from './pages/contacto/contacto.component';
 import { QuienesSomosComponent } from './pages/quienes-somos/quienes-somos.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ContactoComponent,
-    QuienesSomosComponent
+    QuienesSomosComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
